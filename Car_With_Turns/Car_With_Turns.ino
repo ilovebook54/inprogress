c3cc8483069fb2729a018cd1dc9a3dee6885c061
ilovebook54 <42848205+ilovebook54@users.noreply.github.com>
2018-11-11 11:53:38 -0500

    Add files via upload

---
+++ Car_With_Turns/Car_With_Turns.ino
@@ -0,0 +1,70 @@
+#include <IRremote.h>
+#include "pins_arduino.h"
+int RECV_PIN = 11;//Pin should be PWM
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{ 
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);  
+}
+void loop() {
+  if (irrecv.decode(&results)) {
+      switch(results.value)
+      {        
+        case 0xFD50AF:  //Serial.println("Forward"); // Button 2  
+                         digitalWrite(5,LOW);
+                         digitalWrite(6,HIGH);
+                         digitalWrite(7,LOW);
+                         digitalWrite(8,HIGH);
+                         break;       
+        case 0xFD10EF:  //Serial.println("Backward"); // Button 4
+                         digitalWrite(5,HIGH);
+                         digitalWrite(6,LOW);
+                         digitalWrite(7,HIGH);
+                         digitalWrite(8,LOW);
+                         break; 
+        case 0xFD00FF:  //Serial.println("Stop"); // Button 5
+                         digitalWrite(5,HIGH);
+                         digitalWrite(6,HIGH);
+                         digitalWrite(7,HIGH);
+                         digitalWrite(8,HIGH);
+                         break; 
+        case 0xFD8877:  //Serial.println("anti-clockwise"); // Button 6
+                         digitalWrite(5,LOW);
+                         digitalWrite(6,LOW);
+                         digitalWrite(7,HIGH);
+                         digitalWrite(8,LOW);
+                         break;        
+                        
+                         
+        case 0xFD08F7:  //Serial.println("clockwise"); // Button 8  
+                         digitalWrite(5,HIGH);
+                         digitalWrite(6,LOW);
+                         digitalWrite(7,HIGH);
+                         digitalWrite(8,HIGH);
+                         break;  
+
+/*        case FD807F:  //Serial.println("Right"); // Button 8  
+                         int carspeed = ++ ;  
+
+
+
+        case FD906F:  //Serial.println("Right"); // Button 8  
+                         int carspeed = -1 ;  
+*/
+                             
+      }  
+
+      
+      
+    irrecv.resume(); // Receive the next value
+  }
+}
+
---
+++ Wifi/WifiLED/WifiLED.ino
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+int RELAY_PIN = 12;
+const char* password = "";
+const char* ssid = "default";
+const int port = 8181;
+
+WiFiServer server(port);
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+
+  WiFiClient client = server.available();
+  while(client.connected()) {
+    
+    if (client.available()) {
+    
+      uint8_t buf;
+      size_t length = 1;
+      client.read(&buf, length);
+      client.write(handleCmd(buf));
+    }
+  }
+}
+
+char* handleCmd(uint8_t cmd) {
+  Serial.print("Value in ASCII ," );
+  Serial.println(cmd);
+  Serial.print("Value in Character ,");
+  Serial.println(char(cmd));
+  switch(cmd) {
+    case 49:
+   //     Serial.println("In Case 49");
+        digitalWrite(RELAY_PIN, HIGH);
+        return "ON";
+    case 48:
+   //     Serial.println("In Case 48");
+        digitalWrite(RELAY_PIN, LOW);
+        return "OFF";
+    default:
+        return ".";
+  }
+}
+
+
